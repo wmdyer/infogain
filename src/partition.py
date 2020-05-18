e8de6e3420ba1c12cf2f1d654802ff1b58643a5f
William Dyer <wm.e.dyer@gmail.com>
2020-05-18 16:15:22 -0400

    clean up

--- src/partition.py
+++ src/partition.py
@@ -4,13 +4,6 @@ import numpy as np
 from scipy.stats import entropy
 import argparse
 
-def print_progress(i, n):
-    j = (i+1) / n
-    sys.stdout.write('\r')
-    sys.stdout.write("[%-20s] %d%% N %d/%d " % ('='*int(20*j), 100*j, i, n))
-    sys.stdout.flush()
-    return i + 1
-
 def load_triples(filename):
     triples = pd.read_csv(filename, sep=",", header=None)
     return triples
@@ -21,103 +14,104 @@ def load_pairs(filename):
     pairs.dropna(inplace=True)
     return pairs
 
+# pairs is adj-noun pairs, a is adj1, b is adj2, counts is count of adjs, n is noun or None
 def partition(pairs, a, b, counts, n):
 
+    # define whether IG should be a pct of starting entropy
     percentage = True
-    
+
+    # get counts of adjs that overlap adj a
     nouns_a = pairs.loc[pairs['awf'] == a]['nwf']
     adjs_a = pairs.loc[pairs['nwf'].isin(nouns_a)]
     adjs_a = pd.pivot_table(adjs_a[['count', 'awf']], index=['awf'], values=['count', 'awf'], aggfunc=np.sum)
 
+    # get counts of adjs that overlap adj b
     nouns_b = pairs.loc[pairs['awf'] == b]['nwf']
     adjs_b = pairs.loc[pairs['nwf'].isin(nouns_b)]
     adjs_b = pd.pivot_table(adjs_b[['count', 'awf']], index=['awf'], values=['count', 'awf'], aggfunc=np.sum)
 
+    # if noun is specified, change domain to just adjs associated to noun n
     if n != None:
         adjs_n = pairs.loc[pairs['nwf'] == n]['awf']
     else:
-        adjs_n = pairs.loc[(pairs['nwf'].isin(nouns_a.values)) | (pairs['nwf'].isin(nouns_b.values))]['awf']
+        #adjs_n = pairs.loc[(pairs['nwf'].isin(nouns_a.values)) | (pairs['nwf'].isin(nouns_b.values))]['awf'].drop_duplicates()
+        adjs_n = pairs['awf'].drop_duplicates()
 
+    # join adjs_a, adjs_n, and counts
     adjs = pd.merge(adjs_a, adjs_n, how='right', left_on=['awf'], right_on=['awf']).fillna(0).drop_duplicates()
     dfa = pd.merge(adjs, counts, how='left', left_on=['awf'], right_index=True).fillna(0).drop_duplicates()
-        
+
+    # join adjs_b, adjs_n, and counts
     adjs = pd.merge(adjs_b, adjs_n, how='right', left_on=['awf'], right_on=['awf']).fillna(0).drop_duplicates()
     dfb = pd.merge(adjs, counts, how='left', left_on=['awf'], right_index=True).fillna(0).drop_duplicates()
 
     dfa.set_index('awf', inplace=True)
     dfb.set_index('awf', inplace=True)
 
-    try:
-        dfa['pa'] = dfa['count_x']
-        dfa['pac'] = (dfa['count_y'] - dfa['count_x'])
+    dfa['pa'] = dfa['count_x']
+    dfa['pac'] = (dfa['count_y'] - dfa['count_x'])
         
-        dfb['pb'] = dfb['count_x']
-        dfb['pbc'] = (dfb['count_y'] - dfb['count_x'])
+    dfb['pb'] = dfb['count_x']
+    dfb['pbc'] = (dfb['count_y'] - dfb['count_x'])
         
-        dfa['pab'] = dfa['pa'] * (dfb['count_x']/dfb['count_y'])
-        dfa['pabc'] = dfa['pa'] * ((dfb['count_y'] - dfb['count_x'])/dfb['count_y'])
+    dfa['pab'] = dfa['pa'] * (dfb['count_x']/dfb['count_y'])
+    dfa['pabc'] = dfa['pa'] * ((dfb['count_y'] - dfb['count_x'])/dfb['count_y'])
         
-        dfb['pba'] = dfb['pb'] * (dfa['count_x']/dfa['count_y'])
-        dfb['pbac'] = dfb['pb'] * ((dfa['count_y'] - dfa['count_x'])/dfa['count_y'])
-                
-        pa = np.nan_to_num(dfa['pa'].values, 0)
-        pac = np.nan_to_num(dfa['pac'].values, 0)
-        pab = np.nan_to_num(dfa['pab'].values, 0)
-        pabc = np.nan_to_num(dfa['pabc'].values, 0)
-        
-        pb = np.nan_to_num(dfb['pb'].values, 0)
-        pbc = np.nan_to_num(dfb['pbc'].values, 0)
-        pba = np.nan_to_num(dfb['pba'].values, 0)
-        pbac = np.nan_to_num(dfb['pbac'].values, 0)
-        
-        npa = np.sum(pa)
-        npac = np.sum(pac)
-        na = npa + npac
-        
-        npab = np.sum(pab)
-        npabc = np.sum(pabc)
-        nab = npab + npabc
-        
-        npb = np.sum(pb)
-        npbc = np.sum(pbc)
-        nb = npb + npbc
-        
-        npba = np.sum(pba)
-        npbac = np.sum(pbac)
-        nba = npba + npbac
-        
-        ent_a = entropy(dfa['count_y'].values, base=2)
-        ent_b = entropy(dfb['count_y'].values, base=2)
-        
-        ent_ab = entropy(dfa['count_x'].values, base=2)
-        ent_ba = entropy(dfb['count_x'].values, base=2)           
-        
-        Ia = (npa/na) * entropy(list(pa), base=2) + (npac/na) * entropy(list(pac), base=2)
-        iga1 = (ent_a - Ia)
-        
-        Iab = (npab/nab) * entropy(list(pab), base=2) + (npabc/nab) * entropy(list(pabc), base=2)
-        iga2 = (ent_ab - Iab)
-        
-        Ib = (npb/nb) * entropy(list(pb), base=2) + (npbc/nb) * entropy(list(pbc), base=2)
-        igb1 = (ent_b - Ib)
-        
-        Iba = (npba/nba) * entropy(list(pba), base=2) + (npbac/nba) * entropy(list(pbac), base=2)
-        igb2 = (ent_ba - Iba)
+    dfb['pba'] = dfb['pb'] * (dfa['count_x']/dfa['count_y'])
+    dfb['pbac'] = dfb['pb'] * ((dfa['count_y'] - dfa['count_x'])/dfa['count_y'])
+    
+    # get distributions
+    distributions = {}
+    for d in ['pa', 'pac', 'pab', 'pabc']:
+        distributions[d] = np.nan_to_num(dfa[d].values, 0)
+    for d in ['pb', 'pbc', 'pba', 'pbac']:
+        distributions[d] = np.nan_to_num(dfb[d].values, 0)
 
-        if percentage:
-            iga1 = iga1/ent_a
-            iga2 = iga2/ent_ab
-            igb1 = igb1/ent_b
-            igb2 = igb2/ent_ba
+    # get sums and entropies for each distribution
+    sums = {}
+    entropies = {}
+    for d in distributions.keys():
+        sums[d] = np.sum(distributions[d])
+        try:
+            entropies[d] = entropy(distributions[d], base=2)
+        except:
+            entropies[d] = 0
+
+    # get proportions for each distribution
+    proportions = {}
+    proportions['pa'] = sums['pa'] / (sums['pa'] + sums['pac'])
+    proportions['pb'] = sums['pb'] / (sums['pb'] + sums['pbc'])
+    proportions['pab'] = sums['pab'] / (sums['pab'] + sums['pabc'])
+    proportions['pba'] = sums['pba'] / (sums['pba'] + sums['pbac'])
+
+    for d in ['pa', 'pb', 'pab', 'pba']:
+        proportions[d+'c'] = 1 - proportions[d]
+
+    # calc starting entropies
+    start_ents = {}
+    start_ents['a'] = entropy(dfa['count_y'].values, base=2)
+    start_ents['ab'] = entropy(dfa['count_x'].values, base=2)
+    start_ents['b'] = entropy(dfb['count_y'].values, base=2)    
+    start_ents['ba'] = entropy(dfb['count_x'].values, base=2)
+
+    # calc resulting entropies
+    H = {}
+    H['a'] = proportions['pa'] * entropies['pa'] + proportions['pac'] * entropies['pac']
+    H['ab'] = proportions['pab'] * entropies['pab'] + proportions['pabc'] * entropies['pabc']
+    H['b'] = proportions['pb'] * entropies['pb'] + proportions['pbc'] * entropies['pbc']
+    H['ba'] = proportions['pba'] * entropies['pba'] + proportions['pbac'] * entropies['pbac']
+
+    # calc IG (percentage reduction)
+    IG = {}
+    for d in ['a', 'ab', 'b', 'ba']:
+        IG[d] = (start_ents[d] - H[d]) / start_ents[d]
                 
-        return [iga1, iga2, igb1, igb2]
-    except:
-        return [0, 0, 0, 0]
+    return [IG['a'], IG['ab'], IG['b'], IG['ba']]
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='partition')
-    parser.add_argument('-p', '--pairs', nargs=1, dest='pairs', required=True, help='comma-delimited file of count-adj-noun pairs')    
-    parser.add_argument('-t', '--triples', nargs=1, dest='triples', required=True, help='comma-delimited file of adj-adj-noun triples')
+    parser = argparse.ArgumentParser(description='partition adjective space')
+    parser.add_argument('-p', '--pairs', nargs=1, dest='pairs', required=True, help='comma-delimited file of adj-noun pairs [awf,nwf]')    
+    parser.add_argument('-t', '--triples', nargs=1, dest='triples', required=True, help='comma-delimited file of adj-adj-noun triples (no header)')
     args = parser.parse_args()
 
     print("loading pairs from " + args.pairs[0] + " ...")
@@ -127,7 +121,11 @@ if __name__ == '__main__':
     print("loading triples from " + args.triples[0] + " ...")
     triples = load_triples(args.triples[0])
 
-    outfile = args.triples[0].split('_')[0] + "_scores.csv"
+    if 'triples' in args.triples[0]:
+        outfile = args.triples[0].replace('triples', 'scores')
+    else:
+        outfile = "scores.csv"
+        
     outfile = open(outfile, 'w')
 
     total = triples.shape[0]
@@ -149,19 +147,35 @@ if __name__ == '__main__':
             n = row[2]
             order = "abn"
 
-        a = a.split('/')[0]
-        b = b.split('/')[0]
-        n = n.split('/')[0]        
+        # just get lowercase wordforms
+        a = a.split('/')[0].lower()
+        b = b.split('/')[0].lower()
+        n = n.split('/')[0].lower()
 
-        igs_pre = partition(pairs, a, b, counts, None)
-        igs_post = partition(pairs, a, b, counts, n)
-        
-        out = '\t'.join([order, a, b, n] + list(map(str, igs_pre)) + list(map(str, igs_post)))
-            
-        print(str(i+1) + "/" + str(total), out)
-        outfile.write(out + "\n")
+        if a != b:
 
+            if order == "abn" or order == "anb":
+                igs_pre = partition(pairs, a, b, counts, None)
 
+            if order == "anb" or order == "nab":
+                igs_post = partition(pairs, a, b, counts, n)
+
+            # ig(abn) = ig(a-pre) + ig(ab-pre); ig(ban) = ig(b-pre) + ig(ba-pre)
+            if order == "abn":
+                tup = [igs_pre[0] + igs_pre[1], igs_pre[2] + igs_pre[3]]
+
+            # ig(anb) = ig(a-pre) + ig(b-post); ig(bna) = ig(b-pre) + ig(a-post)
+            if order == "anb":
+                tup = [igs_pre[0] + igs_post[2], igs_pre[2] + igs_post[0]]
+
+            # ig(nab) = ig(a-post) + ig(ab-post); ig(nba) = ig(b-post) + ig(ba-post)
+            if order == "nab":
+                tup = [igs_post[0] + igs_post[1], igs_post[2] + igs_post[3]]
+                   
+            out = '\t'.join([order, a, b, n] + list(map(str, tup)))
+                        
+            print(str(i+1) + "/" + str(total), out)
+            outfile.write(out + "\n")
 
     outfile.close()
     
