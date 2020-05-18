7620f059ac792148e03b3134ae17806e8c005bd5
William Dyer <55298952+wmdyer@users.noreply.github.com>
2020-05-18 16:23:18 -0400

    Delete lang_triples.sh

--- tools/ud/lang_triples.sh
+++
@@ -1,6 +0,0 @@
-for lang in `ls | grep UD_ | sed -e 's/UD_//' | cut -d"-" -f1 | sort -u`
-do
-    repos="`ls | grep "UD_"$lang"\-" | tr '\n' ' '`"
-    ../git/tools/ud/extract_ud_triples.sh $repos
-    mv triples.csv $lang"_triples.csv"
-done
