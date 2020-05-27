c34041d4132e0d09baa60f79a76ce39644d85526
William Dyer <wm.e.dyer@gmail.com>
2020-05-26 20:46:09 -0400

    print statuses, allow multiple conllus, use wordforms instead of lemmas

--- tools/extract_conllu_triples.sh
+++ tools/extract_conllu_triples.sh
@@ -6,61 +6,71 @@ fi
 
 printf "" > triples.csv
 
-for file in "$@"
+# if $1 doesn't exist, use $1 to grep through files; else use $@
+if [ "`ls | grep -w "$1"`" == "" ]
+then
+    files="`ls | grep $1`"
+else
+    files="$@"
+fi
+
+for file in $files
 do
     echo $file
 
     # add sentence IDs
-    echo " sents"
+    printf " sents"
     cat $file | grep "^[0-9][0-9]*"$'\t' | gawk 'BEGIN{FS="\t";S=0}{if($1=="1") S++; print S FS $0}' > sents
 
-    # extract all lemmas and key them to sentence:index
-    echo " words"
-    cat sents | gawk 'BEGIN{FS="\t";W=""}{if($3!="_") print $1":"$2 FS $4"/"$5}' > words
+    # extract all wordforms and key them to sentence:index
+    printf " words"
+    cat sents | gawk 'BEGIN{FS="\t";W=""}{if($3!="_") print $1":"$2 FS $3"/"$5}' > words
 
     # extract all nouns
-    echo " all_nouns"
+    printf " all_nouns"
     cat sents | gawk 'BEGIN{FS="\t"}{if($5=="NOUN") print $1":"$2 FS $1":"$8}' > all_nouns
 
     # extract all adjectives
-    echo " all_adjs"
+    printf " all_adjs"
     cat sents | gawk 'BEGIN{FS="\t"}{if($5=="ADJ" && $9=="amod") print $1":"$2 FS $1":"$8}' > all_adjs
 
     # extract all dependent-head pairs
-    echo " children"
+    printf " children"
     cat sents | gawk 'BEGIN{FS="\t"}{print $1":"$2 FS $1":"$8}' > children
 
     # join children and all adjs -- if resulting line has only two fields, it's a childless adj
-    echo " childless"
+    printf " childless"
     join -t$'\t' -1 2 -a2 <(cat children | sort -k2,2) <(cat all_adjs | sort -k1,1) | gawk 'BEGIN{FS="\t"}{if(NF==2) print $0}' > childless
 
     # get nouns with only two dependents
-    echo " noun_2_deps"
+    printf " noun_2_deps"
     join -t$'\t' -1 2 <(cat children | sort -k2,2) <(cat all_nouns | cut -f1 | sort -k1,1) | sort -k1,1 | gawk 'BEGIN{FS="\t";S=""}{if(S!=$1) printf "\n" $1; printf "\t" $2}{S=$1};END{printf "\n"}' | gawk 'BEGIN{FS="\t"}{if(NF==3) print $1}' > nouns_2_deps
     
     # modify childless so its head nouns are only nouns with 2 deps
-    echo " childless"
+    printf " childless"
     join -t$'\t' -2 2 <(cat nouns_2_deps | sort -k1,1) <(cat childless | sort -k2,2) | gawk 'BEGIN{FS="\t"}{print $2 FS $1}' > temp; mv temp childless
 
     # get postnominal adjs
-    echo " ids1"
+    printf " ids1"
     cat childless | gawk 'BEGIN{FS="\t"}{print $2 FS $1}' | sort -k1,1 | gawk 'BEGIN{FS="\t";H=""}{if($1!=H){printf "\n"$1}; printf FS $2}{H=$1}' | gawk 'BEGIN{FS="\t"}{if(NF==3) print $0}' > ids
 
     # get prenominal adjs
-    echo " ids2"
+    printf " ids2"
     cat childless | gawk 'BEGIN{FS="\t"}{print $1 FS $2}' | sort -k1,1 | gawk 'BEGIN{FS="\t";H=""}{if($2!=H){printf "\n"$2}; printf FS $1}{H=$2}' | gawk 'BEGIN{FS="\t"}{if(NF==3) print $0}' >> ids
     
     # join adjs to their head noun and split into triples
-    echo " ids3"
+    printf " ids3"
     cat ids | sort -u | tr '\t' '\n' | tr ':' '\t' | sort -k1,1 -nk2,2 | tr '\t' ':' | gawk 'BEGIN{FS=":";S=""}{if(S!=$1) printf "\n"; printf "\t" $1":"$2}{S=$1};END{printf "\n"}' | sed -e 's/^\t//' | sed -e 's/\([0-9:]*\)\t\([0-9:]*\)\t\([0-9:]*\)\t/\1\t\2\t\3\n/g' | grep ":" > temp; mv temp ids
     
     # make sure word indices are consecutive
-    echo " ids4"
+    printf " ids4"
     cat ids | tr ':' '\t' | gawk 'BEGIN{FS="\t"}{if($2+1 == $4 && $4+1==$6) print $1":"$2 FS $3":"$4 FS $5":"$6}' > temp; mv temp ids
     
     # convert IDs to words and select triples of interest
-    echo " triples1"
+    printf " triples1"
     join -t$'\t' -1 3 <(join -t$'\t' -1 2 <(join -t$'\t' <(cat ids | sort -k1,1) <(cat words | sort -k1,1) | sort -k2,2) <(cat words | sort -k1,1) | sort -k3,3) <(cat words | sort -k1,1) | cut -f4,5,6 | tr '\t' ',' | grep -E "(NOUN.*ADJ.*ADJ)|(ADJ.*NOUN.*ADJ)|(ADJ.*ADJ.*NOUN)" >> triples.csv
+
+    printf "\n"
 done
 
 # verify each row has 3 cols and normalize
@@ -73,9 +83,10 @@ cat triples.csv | grep "ADJ.*NOUN.*ADJ" | wc -l | gawk 'BEGIN{FS="\t"}{print "AN
 cat triples.csv | grep "ADJ.*ADJ.*NOUN" | wc -l | gawk 'BEGIN{FS="\t"}{print "AAN: " $1}'
 
 # clean up
-#rm sents
-#rm all_adjs
-#rm children
-#rm childless
-#rm ids
-#rm words
+rm sents
+rm all_adjs
+rm children
+rm childless
+rm ids
+rm nouns_2_deps
+rm words
