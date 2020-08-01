038c060ce8fc12d485398f0540591162f0f264ba
William Dyer <wm.e.dyer@gmail.com>
2020-07-31 18:36:06 -0600

    refactor

---
+++ src/test.py
@@ -0,0 +1,229 @@
+import sys, argparse, pickle
+import pandas as pd
+import numpy as np
+from scipy.stats import entropy
+from scipy.special import softmax
+from scipy.sparse import csr_matrix, diags
+from math import log2, exp
+from itertools import permutations, chain, combinations
+#from sklearn.metrics.pairwise import cosine_similarity
+from numpy.linalg import norm
+from sklearn.preprocessing import binarize
+
+# print out data from IG calculations
+VERBOSE = False
+
+# use weighted probabilities
+WEIGHTED_PROBS = False
+
+# maximum number of feature vectors (can also be set with -fn argument)
+MAX_VEC_NUM = 20
+
+# maximum non-zero length of feature vectors (can also be set with -fl argument)
+MAX_VEC_LEN = -1
+
+# use full powerset of all possible features, not just attested ones
+POWERSET = False
+
+def print_progress(i, n):
+    j = (i+1) / n
+    sys.stdout.write('\r')
+    sys.stdout.write("[%-20s] %d%% %d/%d" % ('='*int(20*j), np.clip(100*j, 0, 100), i, n))
+    sys.stdout.flush()
+    return i + 1
+
+def load_seqs(filename, cl):
+    seqs = pd.read_csv(filename, sep=",", header=None)
+
+    # generate permutations 'surface' for 'key'
+    seqs['key'] = ""
+    seqs['surface'] = ""
+    
+    for i,row in seqs.iterrows():
+        w0 = row[0].lower() 
+        w1 = row[1].lower()        
+        try:
+            w2 = row[2].lower()
+        except:
+            w2 = None
+
+        try:
+            w0 = cl[w0.split('/')[0]] + "/" + w0.split('/')[1]
+            w1 = cl[w1.split('/')[0]] + "/" + w1.split('/')[1]
+            w2 = cl[w2.split('/')[0]] + "/" + w2.split('/')[1]
+        except:
+            pass
+
+        if w2 != None:
+            key = ','.join(sorted([w0, w1, w2]))
+            surface = ','.join([w0, w1, w2])
+        else:
+            key = ','.join(sorted([w0, w1]))
+            surface = ','.join([w0, w1])            
+        seqs.at[i, 'key'] = key
+        seqs.at[i, 'surface'] = surface
+    try:
+        # handle triples
+        seqs = seqs.groupby(by=[0, 1, 2, 'key', 'surface']).size().reset_index(name='count')
+    except:
+        # handle pairs
+        seqs = seqs.groupby(by=[0, 1, 'key', 'surface']).size().reset_index(name='count')
+        
+    return seqs[['key', 'surface', 'count']]
+
+# main partition routine (wordlist is either adjs or nouns, m is prob dist, a is adjacency matrix, a_prime is how a changes over time, w is word to partition on, and pos is w's part-of-speech)
+def partition(wordlist, a, probs, w, pos):
+    ig = 0
+    
+    if pos == 'adj':
+        # multiply a by adj row and re-sparsify
+        x = wordlist.index(w)
+        a_yes = a.multiply(binarize(a[x])).tocsr()
+            
+    elif pos == 'noun':
+        # multiply a by noun col and re-sparsify
+        vec = np.zeros(a.shape[1])        
+        y = [i for i, x in enumerate(wordlist) if x == w]
+        np.put(vec, y, 1)
+        a_yes = a * diags(vec)
+        #a_yes = a.multiply(binarize(a[:,y]).reshape(-1,1)).tocsr()
+
+    # a_no is whatever's left of 'a' after removing a_yes
+    a_no = a - a_yes
+
+    # sum a's columns and binarize
+    qk = binarize(a.sum(axis=0))[0]
+    pk = {}    
+    pk['no'] = binarize(a_no.sum(axis=0))[0]
+
+    # pk['yes'] is whatever's left of qk after removing pk['no']
+    pk['yes'] = qk - pk['no']
+
+    a_yes = a_yes.multiply(pk['yes'].reshape(-1,1).T).tocsr()
+
+    # for qk and both pk's, multiply by static probs vector, then normalize by sum of vector (Manhattan norm)
+    qk = qk*probs
+
+    if np.sum(qk) > 0:
+        qk = qk/np.sum(qk)    
+        qk_num = len(np.where(qk != 0)[0])
+        for d in ['yes', 'no']:
+            pk[d] = pk[d] * probs            
+            if np.sum(pk[d]) > 0:
+                pk[d] = pk[d]/np.sum(pk[d])            
+                pk_num = len(np.where(pk[d] != 0)[0])
+                # ig is the proportion of pk[d]'s non-zero vectors to qk's non-zero vectors, times the D_KL of pk[d] from qk
+                ig += (pk_num/qk_num) * entropy(pk=pk[d], qk=qk, base=2)
+    
+    if VERBOSE:
+        print('qk', len(np.where(qk!=0)[0]), '\n', a.A, '\n', qk)
+        print('yes', len(np.where(pk['yes']!=0)[0]), '\n', a_yes.A, '\n', pk['yes'])
+        print('no', len(np.where(pk['no']!=0)[0]), '\n', a_no.A, '\n', pk['no'])        
+        print(ig)
+
+    return ig, a_yes
+
+def score(nouns, adjs, a_orig, probs, seqs, cl, outfile):
+    outfile = open(outfile, 'w')
+    outstr = "key\tsurface\ttemplate\tattest\tig_sum\tig_ent\tigs\n"
+    outfile.write(outstr)
+    n = len(seqs.key.unique())
+    
+    print('scoring sequences ...')
+    an = 0    
+    if n < 5:
+        global VERBOSE
+        VERBOSE = True
+
+        if len(nouns) < 20:
+            print(adjs)
+            print(nouns)
+
+    for k,key in enumerate(seqs.key.unique()):
+        if not VERBOSE: print_progress(k+1, n)
+        words = key.split(',')
+
+        analyze = True
+
+        # if words or clusters don't exist in training set, don't analyze
+        for w in words:
+            wf = w.split('/')[0]
+            try:
+                wf = str(cl[wf])
+            except:
+                pass
+
+            if "/adj" in w and wf not in adjs:
+                analyze = False
+            elif "/noun" in w and wf not in nouns:
+                analyze = False
+        if analyze:
+            out = []
+            for perm in list(permutations(words)):
+                igs = []
+                a = a_orig
+                template = ""
+                for w in perm:
+                    wf = w.split('/')[0]
+                    try:
+                        wf = str(cl[wf])
+                    except:
+                        pass
+                            
+                    if VERBOSE:
+                        print('\n'+w)
+                    if "/adj" in w and wf in adjs:
+                        iga, a = partition(adjs, a, probs, wf, 'adj')
+                        igs.append(iga)
+                        template += "A"
+                    if "/noun" in w and wf in nouns:
+                        ign, a = partition(nouns, a, probs, wf, 'noun')
+                        igs.append(ign)
+                        template += "N"
+
+                # if each word has an IG score, calculate entropy of IG scores and append to out string
+                if len(igs) == len(words):
+                    surface = ','.join(perm)                
+                    try:
+                        attest = seqs.loc[seqs['surface'] == surface]['count'].values[0]
+                    except:
+                        attest = 0
+
+                    if template == "NA":
+                        template = "AN"
+
+                    if np.sum(igs) > 0:
+                        ent = entropy(igs, base=2)
+                    else:
+                        ent = 0
+                    
+                    out.append('\t'.join([key, ','.join(perm), template, str(attest), str(np.sum(igs)), str(ent), ','.join(map(str,igs))]) + "\n")
+
+                # only write to outfile if all permutations have scores
+                if len(out) == len(list(permutations(words))):
+                    for line in out:
+                        outfile.write(line)
+                    an += 1
+
+    outfile.close()
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='partition adjective space')
+    parser.add_argument('-s', '--sequences', nargs=1, dest='seqs', required=True, help='comma-delimited file of sequences (no header), e.g., big/ADJ,red/ADJ,box/NOUN')
+    args = parser.parse_args()
+
+    pkl_file = 'ig.pkl'
+    print("load from " + pkl_file)
+    f = open(pkl_file, 'rb')
+    nouns = pickle.load(f)
+    adjs = pickle.load(f)
+    a_orig = pickle.load(f)
+    probs = pickle.load(f)
+    cl = pickle.load(f)        
+
+    print("loading " + args.seqs[0] + " ...")
+    seqs = load_seqs(args.seqs[0], cl)
+
+    score(nouns, adjs, a_orig, probs, seqs, cl, "scores.temp")
+    print('')
+
---
+++ src/train.py
@@ -0,0 +1,233 @@
+import sys, argparse, pickle
+import pandas as pd
+import numpy as np
+from textdistance import levenshtein
+from itertools import repeat
+from scipy.stats import entropy
+from scipy.special import softmax
+from scipy.sparse import csr_matrix, diags
+from math import log2, exp
+from itertools import permutations, chain, combinations
+#from sklearn.metrics.pairwise import cosine_similarity
+from numpy.linalg import norm
+from sklearn.preprocessing import binarize
+
+# print out data from IG calculations
+VERBOSE = False
+
+# use weighted probabilities
+WEIGHTED_PROBS = True
+
+# maximum number of feature vectors (can also be set with -fn argument)
+MAX_VEC_NUM = 20
+
+# maximum non-zero length of feature vectors (can also be set with -fl argument)
+MAX_VEC_LEN = 2
+
+# use full powerset of all possible features, not just attested ones
+POWERSET = False
+
+def print_progress(i, n):
+    j = (i+1) / n
+    sys.stdout.write('\r')
+    sys.stdout.write("[%-20s] %d%% %d/%d" % ('='*int(20*j), np.clip(100*j, 0, 100), i, n))
+    sys.stdout.flush()
+    return i + 1
+
+def load_clusters(filename):
+    clusters = pd.read_csv(filename, sep=",", error_bad_lines=False, engine='python').drop_duplicates()
+    return clusters
+
+def load_nps(filename, clusters, cl):
+    nps = pd.read_csv(filename, sep="\t", error_bad_lines=False, engine='python', header=None)
+    nps.columns = ['count', 'noun', 'adjs']
+    nps['noun'] = nps['noun'].str.lower()
+    nps['adjs'] = nps['adjs'].str.lower()
+
+    # change nouns and adjs to their clusters
+    if len(clusters) > 0:
+        print('converting wordforms to clusters ...')
+        nps = pd.merge(nps, clusters, how='inner', left_on=['noun'], right_on=['wf'])
+        del nps['noun']
+        del nps['wf']        
+        nps = nps.rename(columns={"cl": "noun"})
+        nps = nps.astype({'noun': 'str'})
+
+        total = len(nps)
+        for i,row in nps.iterrows():
+            print_progress(i+1, total)
+            astring = ""
+            try:
+                for w in row['adjs'].split(','):
+                    try:
+                        acl = cl[w]
+                        if len(astring) > 0:
+                            astring += ","
+                        astring += str(acl)
+                    except:
+                        pass
+            except:
+                pass
+
+            nps.at[i, 'adjs'] = astring
+
+        print('')
+        nps = nps.loc[nps['adjs'] != ""]
+
+    # recalculate count column (probably a more efficient way to do this, but this works)
+    nps = nps.reindex(nps.index.repeat(nps['count']))
+    nps = nps.groupby(by=['noun', 'adjs']).size().reset_index(name='count')
+
+    return nps
+
+# helper function that returns a powerset from an iterable
+def get_powerset(iterable, max_vec_len):
+    s = list(iterable)
+    minimum = 1
+    if max_vec_len > -1:
+        maximum = min([len(s) + 1, max_vec_len + 1])
+    else:
+        maximum = len(s) + 1
+    return chain.from_iterable(combinations(s, r) for r in range(minimum, maximum))
+
+# process NPs into probabilities and expanded adjacency matrix
+def process_nps(nps, max_vec_len, max_vec_num):
+    print("processing attested meanings ...")    
+    probs = []
+    pairs = []
+    adjectives = []
+    nouns = []
+
+    # build up adjacency as regular python list, not numpy, for better performance
+    a = []
+
+    # get all adjectives
+    for alist in nps.adjs.unique():
+        for w in alist.split(','):
+            if w not in adjectives:
+                adjectives.append(w)
+
+    # iterate through nouns to expand adjacency and probs
+    total = len(nps.noun.unique())
+    for i,noun in enumerate(nps.noun.unique()):
+        print_progress(i+1, total)
+        df = nps.loc[nps['noun'] == noun]
+
+        # calculate this noun's list of adjectives
+        if POWERSET:
+            noun_adjs = adjectives
+        else:
+            noun_adjs = []
+            for j,row in df.iterrows():
+                for w in row['adjs'].split(','):
+                    if w not in noun_adjs:
+                        noun_adjs.append(w)
+
+        if len(noun_adjs) > 0:
+            for j,row in df.iterrows():
+                vector = [0] * len(adjectives)
+                for adj in row['adjs'].split(','):
+                    vector[adjectives.index(adj)] = 1
+                if WEIGHTED_PROBS:
+                    probs.append(row['count'])
+                else:
+                    probs.append(1)
+                a.append(vector)
+                nouns.append(noun)
+                
+            if max_vec_num != 0:
+                pset = get_powerset(noun_adjs, max_vec_len)
+                j = 0
+            
+                for s in pset:
+                    sl = ','.join(list(s))
+                    if not (df['adjs']==sl).any():
+                        probs.append(0)
+                        vector = [0] * len(adjectives)
+                        for adj in list(s):
+                            vector[adjectives.index(adj)] = 1
+                
+                        a.append(vector)
+                        nouns.append(noun)
+                        j += 1
+                        if max_vec_num > -1 and j > max_vec_num:
+                            break
+
+    print('')
+    print('converting adjacencies to numpy ...')
+    a_orig = np.array(a)
+
+    print('calculating unattested probabilities ...')
+    np_probs = np.array(probs)
+    np_nouns = np.array(nouns)
+    total = len(np.where(np_probs == 0)[0])
+
+    for i,p in enumerate(np.where(np_probs == 0)[0]):
+        if not VERBOSE:
+            print_progress(i+1, total)
+        noun = nouns[p]
+        attested = a_orig[list(np.where((np_nouns == noun) & (np_probs == 1))[0]),:].T
+        if attested.shape[1] > 0:
+
+            # use euclidean distance
+            vec = np.array(a_orig[p])
+            probs[p] = (norm(attested-vec[:,None], axis=0, ord=2).max()/attested.shape[0])**2
+
+            # use cosine similarity
+            #vector = np.broadcast_to(np.array(a_orig[p]), attested.shape)            
+            #probs[p] = cosine_similarity(vector, attested).max()
+
+    print('')
+    print('sparsifying adjacencies ...')
+    a_orig = csr_matrix(binarize(a_orig.T)).tocsr()
+
+    print('normalizing probabilities ...')
+    probs = np.array(probs)
+    probs = probs/np.sum(probs)
+    # probs = softmax(probs)
+
+    return nouns, adjectives, probs, a_orig
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='partition adjective space')
+    parser.add_argument('-n', '--nps', nargs=1, dest='nps', required=True, help='tab-delimited file of [noun<TAB>adj,adj,...] (no header)')    
+    parser.add_argument('-c', '--clusters', nargs=1, dest='clusters', required=False, help='comma-delimited file of wordform, cluster IDs [wf,cl]')
+    parser.add_argument('-fn', '--num_features', nargs=1, dest='fn', required=False, help='max number of meaning vectors per noun; -1 means unlimited; default is 20')
+    parser.add_argument('-fl', '--len_features', nargs=1, dest='fl', required=False, help='max non-zero length of feature vectors; -1 means unlimited; default is 2')    
+    args = parser.parse_args()
+
+    try:
+        print('loading clusters from ' + args.clusters[0] + ' ...')
+        clusters = load_clusters(args.clusters[0])
+    except:
+        clusters = pd.DataFrame()
+
+    cl = dict(clusters.values.tolist())        
+
+    try:
+        MAX_VEC_NUM = int(args.fn[0])
+    except:
+        pass
+
+    try:
+        MAX_VEC_LEN = int(args.fl[0])
+    except:
+        pass    
+    
+
+    print("loading " + args.nps[0] + " ...")
+    nps = load_nps(args.nps[0], clusters, cl)        
+
+    nouns, adjs, probs, a_orig = process_nps(nps, MAX_VEC_LEN, MAX_VEC_NUM)
+
+    pkl_file = 'ig.pkl'
+    print("save to " + pkl_file)
+    f = open(pkl_file, 'wb')
+    pickle.dump(nouns, f)
+    pickle.dump(adjs, f)
+    pickle.dump(a_orig, f)
+    pickle.dump(probs, f)    
+    pickle.dump(cl, f)
+
+    print('done')
+
