77330621e78cb7ea0bb7dfdd67b6286059b270d4
William Dyer <wm.e.dyer@gmail.com>
2020-09-08 10:27:47 -0600

    make clusters average vector of lemma's wordforms

--- src/cluster.py
+++ src/cluster.py
@@ -13,12 +13,10 @@ def print_progress(i, n):
     sys.stdout.flush()
     return i + 1
 
-def load_words(filename):
-    words = pd.read_csv(filename, sep=",", header=None)
-    words.columns = ['wf']
-    words['wf'] = words['wf'].str.lower()
-    words = words.drop_duplicates()
-    return words
+def load_forms(filename):
+    df = pd.read_csv(filename, sep="\t", header=None).drop_duplicates()
+    df.columns = ['lemma', 'wfs']
+    return df
 
 def load_vectors(filename, c):
     df = pd.read_csv(filename, sep='["]* ["]*', header=None, error_bad_lines=False, engine='python', index_col=0)
@@ -45,24 +43,42 @@ def get_clusters(words, vectors, k):
 
     clusters = {}
 
-    for i, word in enumerate(words['wf'].values):
+    for i, word in enumerate(words['lemma'].values):
         clusters[word] = int(labels[i])
 
     return clusters
         
-def extract_vectors(words, vectors):
-    wv = pd.merge(pd.DataFrame(words.wf.unique()), vectors, left_on=[0], right_index=True)
-    word_vectors = np.array(wv[wv.columns[1:]].values).astype(float)
+def combine_vectors(forms, vectors):
 
-    words = pd.DataFrame(wv[0])
-    words.columns = ['wf']
+    single_forms = forms[forms['wfs'].str.contains(',') == False].drop_duplicates()
+    multi_forms = forms[forms['wfs'].str.contains(',', na=False)].drop_duplicates()
 
-    return words, word_vectors
+    print('extracting lemma vectors ...')
+    wv = pd.merge(single_forms, vectors, left_on=['lemma'], right_index=True)
+    c_forms = wv['lemma'].values.tolist()
+    c_vectors = np.array(wv[wv.columns[2:]].values).astype(float).tolist()
+
+    print('combining wordform vectors ...')
+    total = len(multi_forms.lemma.unique())
+    j=1
+    for i,row in multi_forms.iterrows():
+        j = print_progress(j, total)
+        wv = vectors.loc[vectors.index.isin(str(row['wfs']).split(','))]
+        vec = np.sum(wv.values, axis=0)/wv.values.shape[1]
+        c_forms.append(row['lemma'])
+        c_vectors.append(np.squeeze(vec))
+
+    forms = pd.DataFrame(c_forms)
+    vectors = pd.DataFrame(c_vectors)
+
+    forms.columns = ['lemma']
+
+    return forms, vectors
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='k-means cluster words based on embeddings')
     parser.add_argument('-v', '--vectors', nargs=1, dest='vectors', required=True, help='GloVe file containing word vectors')
-    parser.add_argument('-w', '--words', nargs=1, dest='words', required=True, help='file containing words')
+    parser.add_argument('-f', '--forms', nargs=1, dest='forms', required=True, help='file containing tab-delimited [lemma wordforms]')
     parser.add_argument('-k', nargs=1, dest='k', required=False, help='k value for clustering; default is 300')
     parser.add_argument('-c', nargs=1, dest='c', type=float, required=False, help='pct of components to keep after pca; default is 1.0 (all)')
     args = parser.parse_args()
@@ -76,22 +92,23 @@ if __name__ == '__main__':
     except:
         c = 1.0
 
-    print("loading words from " + args.words[0] + " ...")
-    words = load_words(args.words[0])
+    print("loading forms ...")
+    forms = load_forms(args.forms[0])
 
-    print("loading vectors from " + args.vectors[0] + " ...")
+    print("loading vectors ...")
     vectors = load_vectors(args.vectors[0], c)
 
-    print("extracting vectors ...")
-    words, word_vectors = extract_vectors(words, vectors)
+    words, vectors = combine_vectors(forms, vectors)
 
+    print('')
+    
     print("clustering (k=" + str(k) + ") ...")
-    cls = get_clusters(words, word_vectors, k)
-    words['cl'] = words['wf'].map(cls).astype('str')
+    cls = get_clusters(words, vectors, k)
+    words['cl'] = words['lemma'].map(cls).astype('str')
     words = words.loc[words['cl'] != 'nan']
 
     print("printing output to clusters.csv ...")
     outfile = open("clusters.csv", 'w')
-    outfile.write(words.to_csv(columns=['wf', 'cl'], index=False))
+    outfile.write(words.to_csv(columns=['lemma', 'cl'], index=False))
     outfile.close()
     
