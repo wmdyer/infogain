b63f8439bfa03df989b05f5ee26f9c96dd31a66b
William Dyer <wm.e.dyer@gmail.com>
2020-09-09 19:09:48 -0600

    always run

--- tools/cluster.sh
+++ tools/cluster.sh
@@ -9,16 +9,15 @@ then
     exit
 fi
 
-if [ "`ls | grep -w forms`" == "" ]
-then
-    echo "extracting ADJs and NOUNs ..."
-    cat $conllu_file | grep "^[0-9]" | gawk 'BEGIN{FS="\t"}{if($4=="ADJ" || $4=="NOUN") print $3 FS $2}' | grep -vE '(\$)|(\%)|(")' | grep -v "'" | sort -u > words
+#if [ "`ls | grep -w forms`" == "" ]
+#then
+echo "extracting ADJs and NOUNs ..."
+cat $conllu_file | grep "^[0-9]" | gawk 'BEGIN{FS="\t"}{if($4=="ADJ" || $4=="NOUN") print $3 FS $2}' | grep -vE '(\$)|(\%)|(")' | grep -v "'" | sort -u > words
 
-    echo "extracting vectors ..."
-    join <(cat words | tr '\t' '\n' | sort -u) <(cat $vec_file | sort -k1,1) | gawk 'BEGIN{FS=" "}{if(NF==301) print $0}' > vecs
+echo "extracting vectors ..."
+join <(cat words | tr '\t' '\n' | sort -u) <(cat $vec_file | sort -k1,1) | gawk 'BEGIN{FS=" "}{if(NF==301) print $0}' > vecs
 
-    echo "combining forms ..."
-    cat words | gawk 'BEGIN{FS="\t";W=""}{if($1!=W) printf "\n" $1 FS $1; if($1!=$2) printf "," $2}{W=$1}' > forms
-fi
+echo "combining forms ..."
+cat words | gawk 'BEGIN{FS="\t";W=""}{if($1!=W) printf "\n" $1 FS $1; if($1!=$2) printf "," $2}{W=$1}' > forms
 
 python3 ../../src/cluster.py -v vecs -f forms -k $k -c $c
