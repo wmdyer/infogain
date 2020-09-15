05f6e126a8c0acb3371f8193426bdaf552bdaedb
William Dyer <wm.e.dyer@gmail.com>
2020-09-15 12:20:03 -0600

    only cluster 100K most common words

--- tools/cluster.sh
+++ tools/cluster.sh
@@ -9,10 +9,8 @@ then
     exit
 fi
 
-#if [ "`ls | grep -w forms`" == "" ]
-#then
 echo "extracting ADJs and NOUNs ..."
-cat $conllu_file | grep "^[0-9]" | gawk 'BEGIN{FS="\t"}{if($4=="ADJ" || $4=="NOUN") print $3 FS $2}' | grep -vE '(\$)|(\%)|(")' | grep -v "'" | sort -u > words
+cat $conllu_file | grep "^[0-9]" | gawk 'BEGIN{FS="\t"}{if($4=="ADJ" || $4=="NOUN") print $3 FS $2}' | grep -vE '(\$)|(\%)|(")' | grep -v "'" | sort | uniq -c | sort -rn | head -100000 | sed -e 's/^[ ]*[0-9]*[ ]*//' > words
 
 echo "extracting vectors ..."
 join <(cat words | tr '\t' '\n' | sort -u) <(cat $vec_file | sort -k1,1) | gawk 'BEGIN{FS=" "}{if(NF==301) print $0}' > vecs
