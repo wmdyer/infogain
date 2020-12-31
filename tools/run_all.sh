acb26eda755d923bb5afc9c38dcb10cfc6508ee3
William Dyer <wm.e.dyer@gmail.com>
2020-12-31 09:50:23 -0700

    refactor

--- tools/run_all.sh
+++ tools/run_all.sh
@@ -1,16 +1,13 @@
-ak="50000"
-nk="50000"
-
-fn="0"
-fl="0"
 test="true"
-threshold="20000"
+threshold="50000"
 
-if [ "`ls | grep clusters.csv`" == "" ]
-then
-    echo -e "\nCLUSTER (wiki)"
-    ../../tools/cluster.sh conllu.wiki cc.$lang.300.vec $ak $nk 0.1
-fi
+lang="`pwd | rev | cut -d"/" -f1 | rev`"
+
+#if [ "`ls | grep clusters.csv`" == "" ]
+#then
+#    echo -e "\nCLUSTER (wiki)"
+#    ../../tools/cluster.sh conllu.wiki cc.$lang.300.vec $ak $nk 0.1
+#fi
 
 if [ "`ls | grep nps.tsv`" == "" ]
 then
@@ -20,21 +17,23 @@ fi
 
 if [ "`ls | grep ig.pkl`" == "" ]
 then
-    echo -e "\nTRAIN (wiki)"
-    python3 ../../src/train.py -n <(cat nps.tsv | sort -rnk1,1 | head -100000) -c clusters.csv -fn $fn -fl $fl
+    echo -e "\nBUILD FEAT VECS (wiki)"
+    python3 ../../src/build_feature_vecs.py -n <(cat nps.tsv | sort -rnk1,1 | head -100000)
 fi
 
 if [ "`ls | grep ^triples.wiki`" == "" ]
 then
     echo -e "\nGET TRIPLES (wiki)"
     printf "" > triples
-    i="10"
-    while [ "`cat triples | wc -l`" -lt "$threshold" -a "`ls | grep -w "$lang-wikipedia-0$i.conllu"`" != "" ]
+    i="0"
+    while [ "`cat triples | wc -l`" -lt "$threshold" -a "`ls | grep -w "$lang-wikipedia-00$i.conllu"`" != "" ]
     do
-	../../tools/extract_conllu_triples.sh $lang-wikipedia-0$i.conllu
+	echo "triples"
+	../../tools/extract_conllu_triples.sh $lang-wikipedia-00$i.conllu
 	i="`expr $i + 1`"
 	cat triples.csv >> triples
     done
+    
     cat triples | shuf -n $threshold triples > triples.wiki
 fi
 
@@ -43,12 +42,18 @@ then
     echo -e "\nGET TRIPLES (cc)"
     printf "" > triples
     i="0"
-    while [ "`cat triples | wc -l`" -lt "$threshold" -a "`ls | grep -w "$lang-common_crawl-00$i.conllu"`" != "" ]
+    while [ "`cat triples | wc -l`" -lt "$threshold" -a "`ls | grep  -w "$lang-common_crawl-00$i.conllu"`" != "" ]
     do
 	../../tools/extract_conllu_triples.sh $lang-common_crawl-00$i.conllu
 	i="`expr $i + 1`"
 	cat triples.csv >> triples
     done
+    while [ "`cat triples | wc -l`" -lt "$threshold" -a "`ls | grep  -w "$lang-common_crawl-0$i.conllu"`" != "" ]
+    do
+	../../tools/extract_conllu_triples.sh $lang-common_crawl-0$i.conllu
+	i="`expr $i + 1`"
+	cat triples.csv >> triples
+    done    
     cat triples | shuf -n $threshold > triples.cc
 fi
 
@@ -66,14 +71,14 @@ do
     if [ "`ls | grep scores.$t`" == "" ]
     then
 	echo -e "\nTEST TRIPLES ($t)"
-	python3 ../../src/ablate.py -s triples.$t
+	python3 ../../src/partition.py -s triples.$t
 	mv scores.tsv scores.$t
     fi
 done
 
 if [ "$1" == "" ]
 then
-    m="1st_a sum uc_pos c_pos uc_neg c_neg"
+    m="1st_a uc_pos uc_neg"
 else
     m="$1"
 fi
