28cfb52ff56428faae1ac55d69264ac4663ec30d
William Dyer <wm.e.dyer@gmail.com>
2020-12-31 10:51:07 -0700

    clean up

--- tools/count.sh
+++
@@ -1,3 +0,0 @@
-cc="`../../tools/analyze.sh triples cc | grep analyzed | tr -s ' ' | cut -d" " -f2`"
-wiki="`../../tools/analyze.sh triples wiki | grep analyzed | tr -s ' ' | cut -d" " -f2`"
-echo "$cc+$wiki" | bc -l
